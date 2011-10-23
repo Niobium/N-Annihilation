0b14332e2f575eb312e8935432bac0e039226d4a
Jason <jdrake.nz@gmail.com>
2011-10-24 00:59:29 +1300

    Initial commit (BA 7.50)

---
+++ modinfo.lua
@@ -0,0 +1,10 @@
+return {
+  name='Balanced Annihilation',
+  description='Moooooo!',
+  shortname='BA',
+  version='V7.50',
+  mutator='Official',
+  game='Total Annihilation',
+  shortGame='TA',
+  modtype=1,
+}
